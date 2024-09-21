12bb8dbeb9b4039fc05d91285d0242d39d595d83
jeff-tor <jjiaaffuu@hotmail.com>
2024-09-21 20:09:30 +0800

    commit revised version

--- assignment_3/monte_carlo.py
+++ assignment_3/monte_carlo.py
@@ -1,26 +1,29 @@
 import random
 
-def random_around_zero(r):
-    square_width = 2 * r
-    return random.random() * square_width - r
-
 def in_circle(r, x, y):
     distance_from_center = (x * x + y * y) ** 0.5
     return distance_from_center <= r
 
-def number_generator():
-    x = random.uniform(a=1,b=-1)
-    y = random.uniform(a=-1,b=1)
-    return x,y
+def random_throw(r):
+    x = random.uniform(a=r,b=-r)
+    y = random.uniform(a=-r,b=r)
+    return x, y
 
 def monte_carlo_pi(n):
-    n_circle=0
+    """
+    For simplicity purposes, r is set to 1.
+    This is a simulation of random dart throws in a square with a circle inside
+    The probability, multiplied by 4 will equate approximately to 3.14 (Pi)
+    :param n: number of dart throws
+    :return: the Probability in float
+    """
+    r = 1
+    n_circle = 0
     for i in range(n):
-        x,y = number_generator()
-        if in_circle(1,x,y):
+        x, y = random_throw(r)
+        if in_circle(r, x, y):
             n_circle += 1
-
-    p =  n_circle/n * 4.0
+    p = n_circle/n * 4.0
     return p
 
 print(monte_carlo_pi(10))
